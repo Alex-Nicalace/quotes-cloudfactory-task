6f613fdff3123c8f540498fa74b680d8afc88f85
Alex-Nicalace <anicalace@gmail.com>
2025-12-14 21:56:02 +0200

    fix: add abort controller to fetchQuotes

--- src/stores/quotesStore/index.ts
+++ src/stores/quotesStore/index.ts
@@ -8,9 +8,10 @@ class QuotesStore {
   isLoading = false;
   errorMessage = '';
   loadingTimer: null | ReturnType<typeof setInterval> = null;
+  controller: AbortController | null = null;
 
   constructor() {
-    makeAutoObservable(this, { loadingTimer: false });
+    makeAutoObservable(this, { loadingTimer: false, controller: false });
   }
 
   get quotes() {
@@ -21,17 +22,23 @@ class QuotesStore {
   }
 
   fetchQuotes = async () => {
+    this.controller?.abort();
+    this.controller = new AbortController();
     this.isLoading = true;
     this.errorMessage = '';
     try {
-      const data = await getTickers();
+      const data = await getTickers({ signal: this.controller.signal });
       runInAction(() => {
         this.hashQuotes = makeHashTickers(data || [], this.hashQuotes);
         this.isLoading = false;
       });
-    } catch (e: any) {
+    } catch (e) {
+      if (e instanceof Error && e.name === 'AbortError') {
+        return;
+      }
+
       runInAction(() => {
-        this.errorMessage = e.message;
+        this.errorMessage = e instanceof Error ? e.message : String(e);
         this.isLoading = false;
       });
     }
@@ -44,8 +51,8 @@ class QuotesStore {
   };
 
   stopLoadingTimer = () => {
+    this.controller?.abort();
     if (this.loadingTimer) clearInterval(this.loadingTimer);
-    console.log('stop');
   };
 }
 
